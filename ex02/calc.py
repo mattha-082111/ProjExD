94a61030290d715914d6d22a61bcc104837fe08d
Yoshimura <c0b211894c@edu.teu.ac.jp>
2022-06-21 17:20:25 +0900

    追加機能

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,82 +1,47 @@
 import tkinter as tk
+from tkinter.messagebox import showinfo
 from tkinter import ttk
- 
-# Define
-BUTTON = [
-    ['AC', 'B', 'C', '/'],
-    ['7', '8', '9', '*'],
-    ['4', '5', '6', '-'],
-    ['1', '2', '3', '+'],
-    ['0', '00', '.', '=']
-]
- 
-SYMBOL = ['+', '-', '*', '/']
- 
-class Calu(object):
-    def __init__(self, app=None):
-        # Define
-        self.calc_str = '' # 計算用の文字列
- 
-        # Window Setting
-        app.title('電卓') # Window のタイトル
-        app.geometry('300x500') # Window のサイズ
- 
-        # Frame Setting
-        calc_frame = ttk.Frame(app, width=300, height=50) # 計算式と結果用のFrame
-        calc_frame.propagate(False) # サイズが固定される
-        calc_frame.pack(side=tk.TOP, padx=10, pady=20) # 余白の設定
-        button_frame = ttk.Frame(app, width=300, height=450) # 計算ボタン用のFrame
-        button_frame.propagate(False) # サイズが固定される
-        button_frame.pack(side=tk.BOTTOM) # 余白の設定
- 
-        # Parts Setting
-        self.calc_var = tk.StringVar() 
-        self.ans_var = tk.StringVar() 
-        calc_label = tk.Label(calc_frame, textvariable=self.calc_var, font=("",20)) # 計算式用のLabel
-        ans_label = tk.Label(calc_frame, textvariable=self.ans_var, font=("",15)) # 結果用のLabel
-        calc_label.pack(anchor=tk.E) # 右揃えで配置
-        ans_label.pack(anchor=tk.E) # 右揃えで配置
- 
-        for y, row in enumerate(BUTTON, 1): # Buttonの配置
-            for x, num in enumerate(row):
-                button = tk.Button(button_frame, text=num, font=('Time New Roman', 30), width=3, height=1)
-                button.grid(row=y, column=x) # 列や行を指定して配置
-                button.bind('<Button-1>', self.click_button) # Buttonが押された場合
-    
-    def click_button(self, event):
-        check = event.widget['text'] # 押したボタンのCheck
- 
-        if check == '=': # イコールの場合
-            if self.calc_str[-1:] in SYMBOL: # 記号の場合、記号よりも前で計算
-                self.calc_str = self.calc_str[:-1]
- 
-            res = '= ' + str(eval(self.calc_str)) # eval関数の利用
-            self.ans_var.set(res)
-        elif check == 'C': # クリアの場合
-            self.calc_str = ''
-            self.ans_var.set('')
-        elif check == 'B': # バックの場合
-            self.calc_str = self.calc_str[:-1]
-        elif check in SYMBOL: # 記号の場合
-            if self.calc_str[-1:] not in SYMBOL and self.calc_str[-1:] != '':
-                self.calc_str += check
-            elif self.calc_str[-1:] in SYMBOL: # 記号の場合、入れ替える
-                self.calc_str = self.calc_str[:-1] + check
-        else: # 数字などの場合
-            self.calc_str += check
- 
-        self.calc_var.set(self.calc_str)
-    
- 
- 
-def main():
-    # Window Setting
-    app = tk.Tk()
-    # Window size non resizable
-    app.resizable(width=False, height=False)
-    Calu(app)
-    # Display
-    app.mainloop() # Window をループで回すことで Widgit に対応できるようになる
- 
+
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    entry.grid(row=0,column=0,columnspan=3)
+    if num == "=":#計算結果を表示
+        ans = entry.get()
+        print(ans)
+        res = eval(ans)
+        entry.delete(0,tk.END)
+        entry.insert(tk.END,res)
+    elif num == "AC":
+        entry.delete(0,tk.END)
+    else:
+        entry.insert(tk.END,num)
+
 if __name__ == '__main__':
-    main()
+    root = tk.Tk()
+    root.geometry("400x600")
+    root.title("電卓")
+    entry = tk.Entry(root,justify="right",width=10,font=("Time New Roman",40))
+
+    s,t = 1,0
+    for i,num in enumerate(["AC"," "," ",
+                            9, 8, 7,
+                            6, 5, 4,
+                            3, 2, 1,
+                            0,"+","-",
+                            "*","/","="]):
+        btn = tk.Button(root,
+                        text = f"{num}",
+                        width = 4,
+                        height = 2,
+                        font = ("Time New Roman",30)
+                        )
+        btn.bind("<1>", button_click)
+        btn.grid(row=s,column=t)
+        t += 1
+        if(i+1)%4 == 0:
+            s += 1
+            t = 0
+
+
+    root.mainloop()
