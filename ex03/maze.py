1df36adccc138587cfee81362e416dd2881f9d15
Yoshimura <c0b211894c@edu.teu.ac.jp>
2022-06-28 15:52:53 +0900

    ３限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,17 +11,25 @@ def key_up():
     key = ""
 
 def main_proc():
-    global cx, cy
+    global cx, cy,mx,my
     delta = { #押されているキーkey/値:移動幅リスト[x,y]
         ""      :[0,0],
         "Up"    :[0, -1],
         "Down"  :[0, +1],
         "Left"  :[-1, 0],
         "Right" :[+1,0],
+        
     }
-    cx,cy = cx + delta[key][0], cy + delta[key][1]
+    try:
+        #cx,cy = cx + delta[key][0], cy + delta[key][1]
+        
+        if maze_bg[my+delta[key][1]][mx+delta[key][0]] == 0:
+            my,mx = my+delta[key][1],mx+delta[key][0]
+    except:
+        pass
+    cx,cy = 100*mx+50,100*my+50
     canvas.coords("tori", cx, cy)
-    root.after(10, main_proc)
+    root.after(150, main_proc)
 
 
 if __name__ == "__main__":
